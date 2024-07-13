124ad047d4078ed5a9abda3217d3c3d3c7bc8030
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-07-13 16:13:50 -0700

    added lock_results.csv and locks_analysis.ipynb

--- homework/locks_counter_no_lock.c
+++ homework/locks_counter_no_lock.c
@@ -52,6 +52,25 @@ long duration(struct timeval start, struct timeval end) {
     return ((end.tv_sec - start.tv_sec) * MICRO_SECONDS_IN_SECOND) + (end.tv_usec - start.tv_usec);
 }
 
+int record_stats(const char* type, int num_threads, int num_loops, long micro_seconds) {
+    // as a reminder, `const` means that the value of the variable `type` will not be modified
+    // it leads to better safety and optimization by the compiler
+    FILE *file = fopen("results.csv", "a+");
+    if (file == NULL) {
+        fprintf(stderr, "Failed to open results.csv\n");
+        return 1;
+    }
+    // Check to see if the file is empty
+    fseek(file, 0, SEEK_END); // moves file pointer to the end of the file
+    long current_position = ftell(file); // get the current file pointer's position
+    if (current_position == 0) {  // if the position is 0, the file is empty; write headers
+        fprintf(file, "Type, Microseconds, # Threads, # Loops\n");
+    }
+    fseek(file, 0, SEEK_END);  // move the the pointer to the end of the file to append data
+    fprintf(file, "%s, %d, %d, %ld\n", type, num_threads, num_loops, micro_seconds);
+    fclose(file);
+}
+
 int main(int argc, char *argv[]) {
     printf("COUNTER - NO LOCK\n");
     if (argc < 3) {
@@ -87,5 +106,9 @@ int main(int argc, char *argv[]) {
     long micro_seconds = duration(start, end);
     printf("Execution time: %ld microseconds\n", micro_seconds);
     printf("----\n\n");
+    int status = record_stats("No Locking", num_threads, num_loops, micro_seconds);
+    if (status != 0) {
+        return status;
+    }
     return 0;
 }
